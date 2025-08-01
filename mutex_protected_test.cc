0041e1b670cd5524e1129ad72b14c824103787fe
Timo Ewalds <timo@ewalds.ca>
2025-08-01 11:10:26 +0100

    absolute minimal

--- mutex_protected_test.cc
+++ mutex_protected_test.cc
@@ -1,14 +1,10 @@
 #include "mutex_protected.h"
 
 #include <chrono>
-#include <string>
-#include <thread>
-#include <vector>
 
 #include "gtest/gtest.h"
 
 using namespace std::chrono_literals;
-auto now = std::chrono::system_clock::now;
 
 namespace xyz {
 
@@ -16,7 +12,6 @@ TEST(TimedMutexProtectedTest, TimeoutWorksCorrectly) {
   mutex_protected<int, std::timed_mutex> value(1);
 
   auto locked = value.try_lock_for(1ms);
-  // ASSERT_TRUE(locked.owns_lock());
 }
 
 }  // namespace xyz
